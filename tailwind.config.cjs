a6943839eca78b772b0cc8b5b98aad7ecd2392e4
Callum Fallows <callum_fallows@hotmail.co.uk>
2024-11-27 19:54:01 +0000

    init
    Deploying first version

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#11536F',
+        'primary-light': '#2B6191',
+        'title-alt': '#515151',
+        subtitle: '#429CD2',
+      },
+      fontFamily: {
+        sans: ['Mulish', ...defaultTheme.fontFamily.sans],
+        background: {
+          hero: "url('/assets/hero-bg.jpg')",
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+};

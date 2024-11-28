10ddf8ccacf834a1632efa62fd7ad0175e74f621
Callum Fallows <callum_fallows@hotmail.co.uk>
2024-11-28 02:06:24 +0000

    refined repo further

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: 'https://tradingwizard.vercel.app/',
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });

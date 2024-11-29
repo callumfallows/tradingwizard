7f66268a2837ef8487feacc661493d9571d35f11
Callum Fallows <callum_fallows@hotmail.co.uk>
2024-11-29 10:04:57 +0000

    fixing changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
 
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
-import vercelServerless from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tradingwizard.vercel.app/',
   integrations: [tailwind(), mdx(), sitemap(), icon()],
   output: 'server',
-  security: {
-    checkOrigin: false,
-  },
+  adapter: vercel(),
 });

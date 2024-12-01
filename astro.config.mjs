8a9f2f86194b6d55f4e2e58071d0e484da702009
Callum Fallows <callum_fallows@hotmail.co.uk>
2024-12-01 21:22:26 +0000

    Removed the need for an API call

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,15 @@ import icon from 'astro-icon';
 import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 // https://astro.build/config
+// export default defineConfig({
+//   site: 'https://tradingwizard.vercel.app/',
+//   integrations: [tailwind(), mdx(), sitemap(), icon()],
+//   output: 'hybrid',
+// });
+
 export default defineConfig({
+  output: 'hybrid',
   site: 'https://tradingwizard.vercel.app/',
   integrations: [tailwind(), mdx(), sitemap(), icon()],
-  output: 'server',
   adapter: vercel(),
 });
